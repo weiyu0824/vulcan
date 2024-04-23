9da75d93d4953f141338374fda8a5b67bad4cbea
wylin2 <wylin2@c240g5-110209.wisc.cloudlab.us>
2024-04-23 00:05:37 +0000

    add driving preception pipeline backbone

--- driving_preception/main.py
+++ driving_preception/main.py
@@ -5,3 +5,19 @@ knobs = [
     ('voxel_size', [0.1, 0.2, 0.3, 0.4, 0.50]),
     ('model', ['PolintPillars-SECFPN, PolintPillars-SECFPN-fp16', 'SSN-SECOND', 'SSN-RegNet', 'CenterPoint-DCN', 'CenterPoint-Circular-NMS'])
 ]
+
+def get_pipeline_args():
+    return {
+       'ground_removal_factor': 0.1,
+        'voxel_size': 0.1,
+        'model': 'PolintPillars-SECFPN',
+    }
+
+def profile_pipeline():
+    pipe_args = get_pipeline_args()
+
+
+if __name__ == "__main__":
+    result = profile_pipeline()
+
+    print(result)
