dea305c74d08858b2ab40d9ff42128b80135079a
wylin2 <wylin2@c240g5-110119.wisc.cloudlab.us>
2024-04-29 15:47:20 -0500

    add driving perception pipe

--- driving_preception/main.py
+++ driving_preception/main.py
@@ -2,7 +2,7 @@
 
 knobs = [
     ('ground_removal_factor', [0.1, 0.2, 0.3, 0.4, 0.5]),
-    ('voxel_size', [0.1, 0.2, 0.3, 0.4, 0.50]),
+    ('voxel_size', [0.1, 0.2, 0.3, 0.4, 0.5]),
     ('model', ['PolintPillars-SECFPN, PolintPillars-SECFPN-fp16', 'SSN-SECOND', 'SSN-RegNet', 'CenterPoint-DCN', 'CenterPoint-Circular-NMS'])
 ]
 
